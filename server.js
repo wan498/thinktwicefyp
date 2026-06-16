2027a363ce4d26c1b344bafc639a5b93f150a400
wan498 <anuarsafuan92@gmail.com>
2026-06-16 18:03:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.json());
 /* ================= MYSQL POOL ================= */
 
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: Number(process.env.MYSQLPORT),
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
 });
