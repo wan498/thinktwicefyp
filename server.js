cb1d32cb65dd2af4b1f376a36938e8ae90192c86
wan498 <anuarsafuan92@gmail.com>
2026-06-16 18:45:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.json());
 /* ================= MYSQL POOL ================= */
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "thinktwice_db",
+  port: Number(process.env.DB_PORT || 3306),
 });
 
 /* TEST DB */
